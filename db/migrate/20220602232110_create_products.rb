5649be37ec94d73f9a2ee64813f00488ef5d0002
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-02 16:30:41 -0700

    add products table to database

---
+++ db/migrate/20220602232110_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.column(:name, :string)
+      t.column(:year, :integer)
+      
+      t.timestamps()
+    end
+  end
+end
