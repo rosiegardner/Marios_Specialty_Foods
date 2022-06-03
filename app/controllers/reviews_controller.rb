1ce905be6c68ce45d87582fd3364deda5acd829d
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-02 19:25:24 -0700

    add code for updating review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -28,5 +28,15 @@ class ReviewsController < ApplicationController
     render :show
   end
 
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
+    else
+      @product = Product.find(params[:product_id])
+      render :edit
+    end
+  end
+
 
 end
