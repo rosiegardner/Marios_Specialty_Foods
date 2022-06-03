5960d2ac54948cc84c10e6afa69217ad147648e8
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-02 22:33:30 -0700

    replace year column with cost in prodcuts

--- db/migrate/20220602232110_create_products.rb
+++ db/migrate/20220602232110_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       t.column(:name, :string)
-      t.column(:year, :integer)
+      t.column(:cost, :integer)
       
       t.timestamps()
     end
