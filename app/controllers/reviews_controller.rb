248af20613ab7b1a3b48c70f78ed02f5bc508296
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-02 19:18:23 -0700

    add code for creating new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,19 @@
 class ReviewsController < ApplicationController
+
   def new
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new
     render :new
   end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(product)
+    else
+      render :new
+    end
+  end
+
 end
