d9b9f9d54b8bbc6cda60c92e4ddfff923dd47ad3
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-02 23:00:07 -0700

    add code for titleize callback test- pass

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates_length_of :name, maximum: 100
+
+  before_save(:titleize_product)
+
+  private
+    def titleize_product
+      self.name = self.name.titleize
+    end
 end
