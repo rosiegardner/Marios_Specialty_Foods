fea05fd0927b73195154db7ea2cdc45a777bc8a7
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-02 22:38:42 -0700

    update review params private method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,6 +46,7 @@ class ProductsController < ApplicationController
 
   private
     def product_params
-      params.require(:product).permit(:name, :country_of_origin)
+      params.require(:product).permit(:name, :cost, :country_of_origin)
+    end
 
 end
