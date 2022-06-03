17eb9b8c11458f7d8aef2445aad03e40d2d55b51
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-03 11:21:09 -0700

    add code to validate presence of cost - passes test

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates_length_of :name, maximum: 100
+  validates :cost, presence: true
 
   before_save(:titleize_product)
 
