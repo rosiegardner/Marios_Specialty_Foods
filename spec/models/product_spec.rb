e2436b6d7d4342aa036613f1dede6bfc35d38265
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-03 11:20:13 -0700

    add test, validate presence of cost - expect pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,9 +4,11 @@ describe Product do
   it { should have_many(:reviews) }
   it { should validate_presence_of :name }
   it { should validate_length_of(:name).is_at_most(100) }
+  it { should validate_presence_of :cost }
+  # it { should validate_presence_of :country_of_origin }
 
   it("titleizes the name of a product") do
-    product = Product.create({name: "blood orange", country_of_origin: "southern mediterranean"})
+    product = Product.create({name: "blood orange", cost: 2, country_of_origin: "southern mediterranean"})
     expect(product.name()).to(eq("Blood Orange"))
   end
 end
