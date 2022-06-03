e2d2419a6d6fb95c73f4efc5d8a0cbe3d3d8c4fd
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-03 13:57:40 -0700

    add scope for 3 recent added products

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,7 @@ class Product < ApplicationRecord
   before_save(:titleize_product)
 
   scope :most_reviewed, -> {(select("products.id, products.name, products.cost, count(reviews.id) as reviews_count").joins(:reviews).group("products.id").order("reviews_count DESC").limit(1))}
-
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
   private
     def titleize_product
       self.name = self.name.titleize
