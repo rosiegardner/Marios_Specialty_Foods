7b4bbfc0d7eaa256d4e3414c920fed66219076af
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-02 22:57:26 -0700

    add test for titleize - expect fail

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,4 +4,9 @@ describe Product do
   it { should have_many(:reviews) }
   it { should validate_presence_of :name }
   it { should validate_length_of(:name).is_at_most(100) }
+
+  it("titleizes the name of a product") do
+    product = Product.create({name: "blood orange", country_of_origin: "southern mediterranean"})
+    expect(product.name()).to(eq("Blood Orange"))
+  end
 end
