03a7aba74c942654bd1fcdf3ee431a068714ca62
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-03 12:35:56 -0700

    add validation of cost

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates_length_of :name, maximum: 100
   validates :cost, presence: true
+  validates :cost, numericality: { only_integer: true, greater_than: 0 }
   validates :country_of_origin, presence: true
   before_save(:titleize_product)
 
