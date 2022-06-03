bdc68467b2b971d69cbcb2fdc2d78cb1807a60c7
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-02 18:27:05 -0700

    add code for deleting a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -39,6 +39,9 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
   end
 
 end
