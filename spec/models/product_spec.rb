1f7b761161206cbd4f9f9db54fbfdd88f031922a
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-03 11:22:08 -0700

    add test, validate presence of country of origin - expect pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,7 +5,7 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_length_of(:name).is_at_most(100) }
   it { should validate_presence_of :cost }
-  # it { should validate_presence_of :country_of_origin }
+  it { should validate_presence_of :country_of_origin }
 
   it("titleizes the name of a product") do
     product = Product.create({name: "blood orange", cost: 2, country_of_origin: "southern mediterranean"})
