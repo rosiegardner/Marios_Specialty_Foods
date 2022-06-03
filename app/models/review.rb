a561aa471c33274e5b53e06043439ec477b893fd
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-03 12:27:32 -0700

    add code to validate length of content body- passes test

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ApplicationRecord
   validates :author, presence: true
   validates :rating, presence: true
   validates :content_body, presence: true
+  validates :content_body, length: { minimum: 50, maximum: 250 }
 end
