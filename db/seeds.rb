93d44c579daeac68f844c26db5f39438929b1713
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-04 13:26:22 -0700

    add seed data for product reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.destroy_all
+Review.destroy_all
 
+product_id_array = []
 50.times do |index|
   Product.create!(name: Faker::Food.fruits, cost: Faker::Number.between(from: 1, to: 10,), country_of_origin: Faker::Address.country)
+  product_id_array.push(Product.last.id)
+  product_id_array.each do |r_index|
+    5.times do |index|
+      Review.create!(author: Faker::Name.name, rating: Faker::Number.between(from: 1, to: 5), content_body: Faker::Hipster.paragraph_by_chars(characters: 250, supplemental: false), product_id: r_index)
+    end
+  end
 end
 
+p product_id_array
 p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
   
 
