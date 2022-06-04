fc51ad8e3242c813c2b660172626d03ac5b26077
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-03 23:11:25 -0700

    add seed data for 50 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Food.fruits, cost: Faker::Number.between(from: 1, to: 10,), country_of_origin: Faker::Address.country)
+end
+
+p "Created #{Product.count} products"
+  
+
