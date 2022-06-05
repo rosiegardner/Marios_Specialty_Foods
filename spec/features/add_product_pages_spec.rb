18d98b12cc72acb859589ce4b17aa52b3f1e1021
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-04 23:18:53 -0700

    add 2nd test for error content- passes test

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -11,4 +11,10 @@ describe "the add a product process" do
     expect(page).to have_content 'Product successfully added!'
     expect(page).to have_content 'Blood Orange'
   end
+
+  it "gives an error when no name is entered" do
+    visit new_product_path
+    click_on 'Create Product'
+    expect(page).to have_content "Name can't be blank"
+  end
 end
