cb8c1dd372d05fbbe1db6fb219caa25df845bfd1
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-16 14:22:30 -0700

    add before action for admin and user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,9 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!, :only => [:new]
+  before_action :only => [:edit, :update, :destroy] do
+    redirect_to new_user_session_path unless current_user && current_user.admin
+  end
+
 
   def new
     @product = Product.find(params[:product_id])
