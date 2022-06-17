b66c3e604241df2c9d2795b92b6858825aee677c
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-17 10:16:56 -0700

    add before each for admin to update product - passes test

--- spec/features/update_product_pages_spec.rb
+++ spec/features/update_product_pages_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 describe "the add a product process" do
+  before(:each) do
+    visit new_user_registration_path
+    click_on "Sign Up!"
+    fill_in "user_email", :with => "admin@epicodus.com"
+    fill_in "user_password", :with => "epicodus123"
+    fill_in "user_password_confirmation", :with => "epicodus123"
+    click_on "Sign up"
+    dumby_admin = User.where(:email => "admin@epicodus.com").update(:admin => true)
+  end
+
   it "adds a new product" do
     new_product = Product.create({name: "Blood Orange", cost: 2, country_of_origin: "Southern Mediterranean"})
     new_product.save
