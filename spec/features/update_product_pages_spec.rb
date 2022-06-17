4c0aac3cd99e73e0d33068840edbc4d7163d058d
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-17 10:26:20 -0700

    rename tests for editing products

--- spec/features/update_product_pages_spec.rb
+++ spec/features/update_product_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "the add a product process" do
+describe "the edit a product process" do
   before(:each) do
     visit new_user_registration_path
     click_on "Sign Up!"
@@ -11,7 +11,7 @@ describe "the add a product process" do
     dumby_admin = User.where(:email => "admin@epicodus.com").update(:admin => true)
   end
 
-  it "adds a new product" do
+  it "edits a new product" do
     new_product = Product.create({name: "Blood Orange", cost: 2, country_of_origin: "Southern Mediterranean"})
     new_product.save
     visit products_path
