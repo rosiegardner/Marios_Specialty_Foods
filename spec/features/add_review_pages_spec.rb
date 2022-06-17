a03f2cf250d6db2ae50f6cb71f73b790f5a9c94d
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-17 10:18:20 -0700

    add before each for user to add reviews - passes test

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 describe "the add a review process" do
+  before(:each) do
+    visit new_user_registration_path
+    click_on "Sign Up!"
+    fill_in "user_email", :with => "admin@epicodus.com"
+    fill_in "user_password", :with => "epicodus123"
+    fill_in "user_password_confirmation", :with => "epicodus123"
+    click_on "Sign up"
+  end
+  
   it "adds a new review with no content" do
     new_product = Product.new({name: "Blood Orange", cost: 2, country_of_origin: "Southern Mediterranean"})
     new_product.save
